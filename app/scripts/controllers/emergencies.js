95b2638df201f88764a1500b737db8a41c808189
Matias Araya <matias.arayac@gmail.com>
2015-10-03 22:35:40 -0300

    nuevas tablas

--- app/scripts/controllers/emergencies.js
+++ app/scripts/controllers/emergencies.js
@@ -7,5 +7,19 @@
  * Controller of the sbAdminApp
  */
 angular.module('sbAdminApp')
-  .controller('EmergenciesCtrl', function($scope,$position) {
-  });
+	.controller('EmergenciesCtrl', function($scope, DTOptionsBuilder) {
+		//Predefine options for table
+		$scope.dtOptions = DTOptionsBuilder.newOptions().withPaginationType('full_numbers').withDisplayLength(25);
+
+		$scope.users = {};
+
+		$scope.$parent.api.branch.getJsonAll({
+			branchName: 'MONEDA',
+			active: 'all'
+		}).then(function(response) {
+			console.log(response);
+			$scope.users = response;
+		}, function(error) {
+			console.log(error);
+		})
+	});
